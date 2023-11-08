d4cfcdaa697ca786935485a55bcaade110eefa9e
gillesania20 <lorenzgillesania20@gmail.com>
2023-11-08 16:41:29 +0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Listing;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(5)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Listing::create([
+            'title' => 'title one',
+            'tags' => 'tag one, tag two',
+            'company' => 'company one',
+            'location' => 'location one',
+            'email' => 'email@email.com',
+            'website' => 'website one',
+            'description' => 'description one'
+        ]);
+        Listing::create([
+            'title' => 'title two',
+            'tags' => 'tag one, tag two',
+            'company' => 'company two',
+            'location' => 'location two',
+            'email' => 'email@email.com',
+            'website' => 'website two',
+            'description' => 'description two'
+        ]);
+    }
+}
